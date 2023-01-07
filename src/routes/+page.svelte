354820daa02f0c8e014858ce059c2b4b912f1b31
chhiring <chhiring@supremeitsolutions.com>
2023-01-07 14:43:15 +0545

    Initial comment

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { tick } from "svelte";
+
+
+	export let foo;
+
+	console.log({ foo });
+
+	export const thisIs = 'readonly';
+
+	export function greet(name) {
+		alert(`hello ${name}`);
+	}
+
+    export let format = n => n.toFixed(2)
+</script>
+
+<style>
+	/* style goes here */
+</style>
